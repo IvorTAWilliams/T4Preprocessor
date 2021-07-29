9bb87d0f25bf5a6ac04e8ca41b96b356005a2431
tom.williams@codebots.com <9aBfd3aZpp>
2021-07-29 12:29:02 +1000

    initial

---
+++ Parsing/BlockType.cs
@@ -0,0 +1,11 @@
+namespace T4Preprocessor.Parsing
+{
+	public enum BlockType
+	{
+		TextBlock,
+		StandardControlBlock,
+		ExpressionControlBlock,
+		Directive,
+		ClassFeatureControlBlock
+	}
+}
---
+++ Parsing/DirectiveProcessorBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace T4Preprocessor.Parsing
+{
+	public abstract class DirectiveProcessorBase
+	{
+		private static readonly string[] _empty = new string[0];
+		private static readonly IncludeFile[] _emptyIncludeFiles = new IncludeFile[0];
+
+		public virtual void Initialize(ITextTemplatingEngineHost host)
+		{
+		}
+
+		public virtual bool IsDirectiveSupported(string directiveName) => false;
+		public abstract void ProcessDirective(string directiveName, IDictionary<string, string> arguments);
+		public virtual string[] GetImportsForProcessingRun() => _empty;
+		public virtual string[] GetReferencesForProcessingRun() => _empty;
+		public virtual IncludeFile[] GetIncludeFilesForProcessingRun() => _emptyIncludeFiles;
+	}
+}
